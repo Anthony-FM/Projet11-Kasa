8458a360efbc2015ea5424b1c8a86b544e2eba01
Anthony Fouda Many <anthony.foudamany@gmail.com>
2023-05-16 09:53:52 +0200

    modif HashRouter

--- kasa/src/index.js
+++ kasa/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom'
 
 // Mes pages
 import Home from './pages/Home';
